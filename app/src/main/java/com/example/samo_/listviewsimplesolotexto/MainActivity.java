f0392fd3aa92df1c9577ee7487aeb5910f2f4774
Riosfen <Riosfen@gmail.com>
2017-01-22 03:59:37 +0100

    ahora tambien tiene acciones

--- app/src/main/java/com/example/samo_/listviewsimplesolotexto/MainActivity.java
+++ app/src/main/java/com/example/samo_/listviewsimplesolotexto/MainActivity.java
@@ -3,7 +3,11 @@ package com.example.samo_.listviewsimplesolotexto;
 import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 // debe extender de ListActivity
@@ -18,5 +22,19 @@ public class MainActivity extends ListActivity {
 
         this.setListAdapter(new ArrayAdapter<>(this,R.layout.layout_listview,R.id.textView_texto,contenido));
 
+        // como la clase extiende de ListActivity, la misma clase tiene el ListView,
+        // asi que llamamos a la clase y hacemos un getListView() para obtener la lista.
+        ListView lista = this.getListView();
+
+        // ahora la podemos tratar y asignarle acciones a los item.
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                Toast.makeText(MainActivity.this, adapterView.getItemAtPosition(i).toString(), Toast.LENGTH_LONG).show();
+
+            }
+        });
     }
 }
+////////////////////////////////////////////////////////////////////////////////////////////////////
